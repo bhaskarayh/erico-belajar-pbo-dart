be4cd16134d56736fe9686b382e087aab61199a9
bhaskarayh <bhaskarayh@gmail.com>
2023-08-17 18:02:03 +0700

    add Inheritance / Turunan + Keyword as & is

---
+++ bin/pertemuan_8.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:my_first_dart_console/hero.dart';
+import 'package:my_first_dart_console/monster.dart';
+import 'package:my_first_dart_console/monster_kecoa.dart';
+import 'package:my_first_dart_console/monster_ubur_ubur.dart';
+
+main(List<String> arguments) async {
+  Hero h = Hero();
+  // Monster m = Monster();
+  Monster monster = MonsterUburUbur();
+  MonsterUburUbur u = MonsterUburUbur();
+  List<Monster> monsters = [];
+
+  monsters.add(MonsterUburUbur());
+  monsters.add(MonsterKecoa());
+  monsters.add(MonsterUburUbur());
+
+  print((monster as MonsterUburUbur).swim());
+
+  // for (Monster m in monsters) {
+  //   // if (m is MonsterUburUbur) {
+  //   //   print(m.swim());
+  //   // }
+  // }
+
+  /* // Setter
+  h.healthPoint = -10;
+  u.healthPoint = 3;
+
+  // Getter
+  print("Hero HP: " + h.healthPoint.toString());
+  print("Monster HP: " + u.healthPoint.toString());
+  print(h.killAMonster());
+  print(u.eatHuman());
+  print(u.swim()); */
+}
---
+++ lib/monster_ubur_ubur.dart
@@ -0,0 +1,5 @@
+import 'package:my_first_dart_console/monster.dart';
+
+class MonsterUburUbur extends Monster {
+  String swim() => "woosh ... woosh ...";
+}
